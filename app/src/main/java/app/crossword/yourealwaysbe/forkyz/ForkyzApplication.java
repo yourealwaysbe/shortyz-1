47919a84048068f1534645317748675d769c5015
Matthew Hague <matthewhague@zoho.com>
2020-06-06 12:20:11 +0100

    Remove tabs from ForkyzApplication

--- app/src/main/java/app/crossword/yourealwaysbe/forkyz/ForkyzApplication.java
+++ app/src/main/java/app/crossword/yourealwaysbe/forkyz/ForkyzApplication.java
@@ -32,22 +32,22 @@ import okhttp3.CookieJar;
 
 public class ForkyzApplication extends Application {
 
-	public static String PUZZLE_DOWNLOAD_CHANNEL_ID = "forkyz.downloads";
+    public static String PUZZLE_DOWNLOAD_CHANNEL_ID = "forkyz.downloads";
     private static ForkyzApplication INSTANCE;
-	private Playboard board;
+    private Playboard board;
     private File baseFile;
-	private PlayboardRenderer renderer;
-	public static File DEBUG_DIR;
-	public static File CROSSWORDS = new File(
-			Environment.getExternalStorageDirectory(), "crosswords");
-	private SharedPreferences settings;
-	private AtomicReference<PersistentCookieJar> cookieJar = new AtomicReference<>(null);
+    private PlayboardRenderer renderer;
+    public static File DEBUG_DIR;
+    public static File CROSSWORDS = new File(
+            Environment.getExternalStorageDirectory(), "crosswords");
+    private SharedPreferences settings;
+    private AtomicReference<PersistentCookieJar> cookieJar = new AtomicReference<>(null);
 
     /**
      * Set the board and base file of the puzzle loaded on it
      */
-	public void setBoard(Playboard board, File baseFile){
-	    this.board = board;
+    public void setBoard(Playboard board, File baseFile){
+        this.board = board;
         this.baseFile = baseFile;
     }
 
@@ -76,7 +76,7 @@ public class ForkyzApplication extends Application {
     }
 
     public void setRenderer(PlayboardRenderer renderer){
-	    this.renderer = renderer;
+        this.renderer = renderer;
     }
 
     public PlayboardRenderer getRenderer() {
@@ -84,92 +84,92 @@ public class ForkyzApplication extends Application {
     }
 
     @Override
-	public void onCreate() {
-		INSTANCE = this;
-		// Initialize credentials and service object.
-		settings = PreferenceManager.getDefaultSharedPreferences(this);
-		super.onCreate();
+    public void onCreate() {
+        INSTANCE = this;
+        // Initialize credentials and service object.
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        super.onCreate();
 
-		AndroidVersionUtils.Factory.getInstance().createNotificationChannel(this);
+        AndroidVersionUtils.Factory.getInstance().createNotificationChannel(this);
 
-		if (Environment.MEDIA_MOUNTED.equals(Environment
-				.getExternalStorageState())) {
-			IO.TEMP_FOLDER = new File(CROSSWORDS, "temp");
-			if(!IO.TEMP_FOLDER.mkdirs()){
-				return;
-			}
-			DEBUG_DIR = new File(CROSSWORDS, "debug");
-			if(!DEBUG_DIR.mkdirs()){
-				return;
-			}
-			File info = new File(DEBUG_DIR, "device");
-			PrintWriter writer = null;
-			try {
-				writer = new PrintWriter(new FileWriter(info));
-				writer.println("VERSION INT: "
-						+ android.os.Build.VERSION.SDK_INT);
-				writer.println("VERSION STRING: "
-						+ android.os.Build.VERSION.SDK);
-				writer.println("VERSION RELEASE: "
-						+ android.os.Build.VERSION.RELEASE);
-				writer.println("MODEL: " + android.os.Build.DEVICE);
-				writer.println("DISPLAY: " + android.os.Build.DISPLAY);
-				writer.println("MANUFACTURER: " + android.os.Build.MANUFACTURER);
-				writer.close();
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			} finally {
-				if(writer != null){
-					writer.close();
-				}
-			}
-		}
-	}
+        if (Environment.MEDIA_MOUNTED.equals(Environment
+                .getExternalStorageState())) {
+            IO.TEMP_FOLDER = new File(CROSSWORDS, "temp");
+            if(!IO.TEMP_FOLDER.mkdirs()){
+                return;
+            }
+            DEBUG_DIR = new File(CROSSWORDS, "debug");
+            if(!DEBUG_DIR.mkdirs()){
+                return;
+            }
+            File info = new File(DEBUG_DIR, "device");
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter(new FileWriter(info));
+                writer.println("VERSION INT: "
+                        + android.os.Build.VERSION.SDK_INT);
+                writer.println("VERSION STRING: "
+                        + android.os.Build.VERSION.SDK);
+                writer.println("VERSION RELEASE: "
+                        + android.os.Build.VERSION.RELEASE);
+                writer.println("MODEL: " + android.os.Build.DEVICE);
+                writer.println("DISPLAY: " + android.os.Build.DISPLAY);
+                writer.println("MANUFACTURER: " + android.os.Build.MANUFACTURER);
+                writer.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                if(writer != null){
+                    writer.close();
+                }
+            }
+        }
+    }
 
 
-	public static void zipDir(String dir2zip, ZipOutputStream zos) {
-		FileInputStream fis = null;
-		try {
-			File zipDir = new File(dir2zip);
-			String[] dirList = zipDir.list();
-			byte[] readBuffer = new byte[2156];
-			int bytesIn = 0;
-			for (int i = 0; i < dirList.length; i++) {
-				File f = new File(zipDir, dirList[i]);
-				if (f.isDirectory()) {
-					String filePath = f.getPath();
-					zipDir(filePath, zos);
-					continue;
-				}
-				try {
-					fis = new FileInputStream(f);
+    public static void zipDir(String dir2zip, ZipOutputStream zos) {
+        FileInputStream fis = null;
+        try {
+            File zipDir = new File(dir2zip);
+            String[] dirList = zipDir.list();
+            byte[] readBuffer = new byte[2156];
+            int bytesIn = 0;
+            for (int i = 0; i < dirList.length; i++) {
+                File f = new File(zipDir, dirList[i]);
+                if (f.isDirectory()) {
+                    String filePath = f.getPath();
+                    zipDir(filePath, zos);
+                    continue;
+                }
+                try {
+                    fis = new FileInputStream(f);
 
-					ZipEntry anEntry = new ZipEntry(f.getPath());
-					zos.putNextEntry(anEntry);
-					while ((bytesIn = fis.read(readBuffer)) != -1) {
-						zos.write(readBuffer, 0, bytesIn);
-					}
-				} finally {
-					if(fis != null){
-						try {
-							fis.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                    ZipEntry anEntry = new ZipEntry(f.getPath());
+                    zos.putNextEntry(anEntry);
+                    while ((bytesIn = fis.read(readBuffer)) != -1) {
+                        zos.write(readBuffer, 0, bytesIn);
+                    }
+                } finally {
+                    if(fis != null){
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static boolean isLandscape(DisplayMetrics metrics){
-		return metrics.widthPixels > metrics.heightPixels;
-	}
+    public static boolean isLandscape(DisplayMetrics metrics){
+        return metrics.widthPixels > metrics.heightPixels;
+    }
 
-	public static boolean isTabletish(DisplayMetrics metrics) {
-		if(android.os.Build.VERSION.SDK_INT > 12) {
+    public static boolean isTabletish(DisplayMetrics metrics) {
+        if(android.os.Build.VERSION.SDK_INT > 12) {
 
             double x = Math.pow(metrics.widthPixels / metrics.xdpi, 2);
             double y = Math.pow(metrics.heightPixels / metrics.ydpi, 2);
@@ -180,44 +180,44 @@ public class ForkyzApplication extends Application {
                 return false;
             }
         } else {
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 
-	public static boolean isMiniTabletish(DisplayMetrics metrics) {
-		switch (android.os.Build.VERSION.SDK_INT) {
-		case 12:
-		case 11:
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-			double x = Math.pow(metrics.widthPixels/metrics.xdpi,2);
-		    double y = Math.pow(metrics.heightPixels/metrics.ydpi,2);
-		    double screenInches = Math.sqrt(x+y);
-			if (screenInches > 5.5 && screenInches <= 9) {
-				return true;
-			} else {
-				return false;
-			}
-		default:
-			return false;
-		}
-	}
+    public static boolean isMiniTabletish(DisplayMetrics metrics) {
+        switch (android.os.Build.VERSION.SDK_INT) {
+        case 12:
+        case 11:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+            double x = Math.pow(metrics.widthPixels/metrics.xdpi,2);
+            double y = Math.pow(metrics.heightPixels/metrics.ydpi,2);
+            double screenInches = Math.sqrt(x+y);
+            if (screenInches > 5.5 && screenInches <= 9) {
+                return true;
+            } else {
+                return false;
+            }
+        default:
+            return false;
+        }
+    }
 
-	public SharedPreferences getSettings() {
-		return settings;
-	}
+    public SharedPreferences getSettings() {
+        return settings;
+    }
 
-	public CookieJar getCookieJar(){
-		if(this.cookieJar.get() == null){
-			this.cookieJar.compareAndSet(null,
-			new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this)));
-		}
-		return this.cookieJar.get();
-	}
+    public CookieJar getCookieJar(){
+        if(this.cookieJar.get() == null){
+            this.cookieJar.compareAndSet(null,
+            new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this)));
+        }
+        return this.cookieJar.get();
+    }
 
-	public static ForkyzApplication getInstance(){
-		return INSTANCE;
-	}
+    public static ForkyzApplication getInstance(){
+        return INSTANCE;
+    }
 }
