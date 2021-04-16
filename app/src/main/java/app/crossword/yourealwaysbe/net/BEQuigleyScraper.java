ab5668f8f6261bc8c818918b64a161bdae3f4bba
Matthew Hague <matthewhague@zoho.com>
2021-04-16 14:06:13 +0100

    Remove (more) unused downloaders

--- app/src/main/java/app/crossword/yourealwaysbe/net/BEQuigleyScraper.java
+++
@@ -1,7 +0,0 @@
-package app.crossword.yourealwaysbe.net;
-
-public class BEQuigleyScraper extends AbstractPageScraper {
-    public BEQuigleyScraper() {
-        super("http://www.fleetwoodwack.typepad.com/", "Brendan Emmett Quigley");
-    }
-}
