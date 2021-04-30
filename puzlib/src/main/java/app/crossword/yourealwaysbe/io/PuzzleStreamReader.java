62c981d6dbda4db7b9a7ae914d67be991423c82b
Matthew Hague <matthewhague@zoho.com>
2021-04-30 21:09:13 +0100

    Make Uclick more robust and and to stream parsers

--- puzlib/src/main/java/app/crossword/yourealwaysbe/io/PuzzleStreamReader.java
+++ puzlib/src/main/java/app/crossword/yourealwaysbe/io/PuzzleStreamReader.java
@@ -7,7 +7,7 @@ import app.crossword.yourealwaysbe.puz.Puzzle;
 
 public class PuzzleStreamReader {
     private static final PuzzleParser[] PARSERS = {
-        new IO(), new JPZIO(), new BrainsOnlyIO()
+        new IO(), new JPZIO(), new UclickXMLIO(), new BrainsOnlyIO()
     };
 
     public interface InputStreamSupplier {
