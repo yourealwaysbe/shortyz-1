54206364f075922430e282416d7b637cf2cbce76
Matthew Hague <matthewhague@zoho.com>
2021-05-24 14:05:17 +0100

    Change backend file format to IPuz

--- app/src/main/java/app/crossword/yourealwaysbe/util/files/PuzHandle.java
+++ app/src/main/java/app/crossword/yourealwaysbe/util/files/PuzHandle.java
@@ -1,25 +1,72 @@
 
 package app.crossword.yourealwaysbe.util.files;
 
-public class PuzHandle {
-    public DirHandle dirHandle;
-    public FileHandle puzHandle;
-    public FileHandle metaHandle;
+import java.io.IOException;
 
-    public PuzHandle(
-        DirHandle dirHandle, FileHandle puzHandle, FileHandle metaHandle
+public abstract class PuzHandle {
+    public DirHandle dirHandle;
+    public FileHandle mainHandle;
+
+    private PuzHandle(
+        DirHandle dirHandle, FileHandle mainHandle
     ) {
         this.dirHandle = dirHandle;
-        this.puzHandle = puzHandle;
-        this.metaHandle = metaHandle;
+        this.mainHandle = mainHandle;
     }
 
     public DirHandle getDirHandle() { return dirHandle; }
-    public FileHandle getPuzFileHandle() { return puzHandle; }
-    public FileHandle getMetaFileHandle() { return metaHandle; }
+    public FileHandle getMainFileHandle() { return mainHandle; }
 
-    // deliberately package-level
-    void setMetaFileHandle(FileHandle metaHandle) {
-        this.metaHandle = metaHandle;
+    public abstract <Ret> Ret accept(Visitor<Ret> v);
+    public abstract <Ret> Ret accept(VisitorIO<Ret> v) throws IOException;
+
+    static class Puz extends PuzHandle {
+        private FileHandle metaHandle;
+
+        public Puz(
+            DirHandle dirHandle, FileHandle mainHandle, FileHandle metaHandle
+        ) {
+            super(dirHandle, mainHandle);
+            this.metaHandle = metaHandle;
+        }
+
+        public FileHandle getMetaFileHandle() { return metaHandle; }
+
+        // deliberately package-level
+        void setMetaFileHandle(FileHandle metaHandle) {
+            this.metaHandle = metaHandle;
+        }
+
+        @Override
+        public <Ret> Ret accept(Visitor<Ret> v) { return v.visit(this); }
+
+        @Override
+        public <Ret> Ret accept(VisitorIO<Ret> v) throws IOException {
+            return v.visit(this);
+        }
+    }
+
+    static class IPuz extends PuzHandle {
+        public IPuz(DirHandle dirHandle, FileHandle mainHandle) {
+            super(dirHandle, mainHandle);
+        }
+
+        @Override
+        public <Ret> Ret accept(Visitor<Ret> v) { return v.visit(this); }
+
+        @Override
+        public <Ret> Ret accept(VisitorIO<Ret> v) throws IOException {
+            return v.visit(this);
+        }
+    }
+
+    static interface Visitor<Ret> {
+        Ret visit(Puz puzHandle);
+        Ret visit(IPuz ipuzHandle);
+    }
+
+    static interface VisitorIO<Ret> {
+        Ret visit(Puz puzHandle) throws IOException;
+        Ret visit(IPuz ipuzHandle) throws IOException;
     }
 }
